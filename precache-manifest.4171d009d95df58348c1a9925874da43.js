add0643d299d8818952e54ff1168511f5e248e85
Justin Luoma <justin@justinluoma.com>
2018-11-02 08:31:48 -0500

    Updated package versions

---
+++ precache-manifest.4171d009d95df58348c1a9925874da43.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b760f6337a8c753a7685",
+    "url": "/static/css/main.c35fcdd5.chunk.css"
+  },
+  {
+    "revision": "b760f6337a8c753a7685",
+    "url": "/static/js/main.b760f633.chunk.js"
+  },
+  {
+    "revision": "d279a0a911453a6a8aa0",
+    "url": "/static/js/1.d279a0a9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ea5eb5f40ecf392011f79f07fa539c5b",
+    "url": "/index.html"
+  }
+];
